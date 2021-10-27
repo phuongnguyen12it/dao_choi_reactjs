8af6a9cf35ed7d6cfcc534dedaefd4a66f1683e0
PhuongNV <vanphuong.nguyen@rikai.technology>
2021-10-27 16:54:48 +0700

    add layout

---
+++ src/component/Home.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component
+{
+    render() {
+        return (
+            <div className="container">
+                
+                <table class="table table-dark mt-4">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Price</th>
+                            <th scope="col" className="th--action"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td><button type="button" class="btn btn-primary btn-sm">add cart</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            <td><button type="button" class="btn btn-primary btn-sm">add cart</button></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                            <td><button type="button" class="btn btn-primary btn-sm">add cart</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
